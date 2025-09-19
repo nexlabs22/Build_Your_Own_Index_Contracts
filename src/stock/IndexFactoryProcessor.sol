0791aa1c2455a3b319264bc6e4656e43514d2d33
Soheil Parsaei <106927616+Soheilprs@users.noreply.github.com>
2025-09-19 03:44:07 +0330

    Added stock smart contracts

---
+++ src/stock/IndexFactoryProcessor.sol
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "../token/IndexToken.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+import "./dinari/interfaces/IOrderProcessor.sol";
+import {FeeLib} from "./dinari/common/FeeLib.sol";
+import "./NexVault.sol";
+import "./dinari/WrappedDShare.sol";
+import "./IndexFactoryStorage.sol";
+import "./OrderManager.sol";
+import "./FunctionsOracle.sol";
+
+/// @title Index Token Factory
+/// @author NEX Labs Protocol
+/// @notice Allows User to initiate burn/mint requests and allows issuers to approve or deny them
+contract IndexFactoryProcessor is Initializable, OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable {
+    IndexFactoryStorage public factoryStorage;
+    FunctionsOracle public functionsOracle;
+
+    event Issuanced(
+        uint256 indexed nonce,
+        address indexed user,
+        address inputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 price,
+        uint256 time
+    );
+
+    event IssuanceCancelled(
+        uint256 indexed nonce,
+        address indexed user,
+        address inputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 time
+    );
+
+    event Redemption(
+        uint256 indexed nonce,
+        address indexed user,
+        address outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 price,
+        uint256 time
+    );
+
+    event RedemptionCancelled(
+        uint256 indexed nonce,
+        address indexed user,
+        address outputToken,
+        uint256 inputAmount,
+        uint256 outputAmount,
+        uint256 time
+    );
+
+    function initialize(address _factoryStorage, address _functionsOracle) external initializer {
+        require(_factoryStorage != address(0), "invalid factory storage address");
+        require(_functionsOracle != address(0), "invalid functions oracle address");
+        factoryStorage = IndexFactoryStorage(_factoryStorage);
+        functionsOracle = FunctionsOracle(_functionsOracle);
+
+        __Ownable_init(msg.sender);
+        __Pausable_init();
+        __ReentrancyGuard_init();
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function setFunctionsOracle(address _functionsOracle) external onlyOwner returns (bool) {
+        require(_functionsOracle != address(0), "invalid functions oracle address");
+        functionsOracle = FunctionsOracle(_functionsOracle);
+        return true;
+    }
+
+    function setIndexFactoryStorage(address _factoryStorage) external onlyOwner returns (bool) {
+        require(_factoryStorage != address(0), "invalid factory storage address");
+        factoryStorage = IndexFactoryStorage(_factoryStorage);
+        return true;
+    }
+
+    function completeIssuance(uint256 _issuanceNonce) public nonReentrant whenNotPaused {
+        require(factoryStorage.checkIssuanceOrdersStatus(_issuanceNonce), "Orders are not completed");
+        require(!factoryStorage.issuanceIsCompleted(_issuanceNonce), "Issuance is completed");
+        address requester = factoryStorage.issuanceRequesterByNonce(_issuanceNonce);
+        IOrderProcessor issuer = factoryStorage.issuer();
+        uint256 primaryPortfolioValue;
+        uint256 secondaryPortfolioValue;
+        for (uint256 i; i < functionsOracle.totalCurrentList(); i++) {
+            address tokenAddress = functionsOracle.currentList(i);
+            uint256 tokenRequestId = factoryStorage.issuanceRequestId(_issuanceNonce, tokenAddress);
+            uint256 price = factoryStorage.priceInWei(tokenAddress);
+            uint256 balance = issuer.getReceivedAmount(tokenRequestId);
+            uint256 receivedValue = balance * price / 1e18;
+            uint256 primaryBalance = factoryStorage.issuanceTokenPrimaryBalance(_issuanceNonce, tokenAddress);
+            uint256 primaryValue = primaryBalance * price / 1e18;
+            uint256 secondaryValue = primaryValue + receivedValue;
+            primaryPortfolioValue += primaryValue;
+            secondaryPortfolioValue += secondaryValue;
+            OrderManager orderManager = factoryStorage.orderManager();
+            orderManager.withdrawFunds(tokenAddress, address(this), balance);
+            IERC20(tokenAddress).approve(factoryStorage.wrappedDshareAddress(tokenAddress), balance);
+            WrappedDShare(factoryStorage.wrappedDshareAddress(tokenAddress)).deposit(
+                balance, address(factoryStorage.vault())
+            );
+        }
+        uint256 primaryTotalSupply = factoryStorage.issuanceIndexTokenPrimaryTotalSupply(_issuanceNonce);
+        if (primaryTotalSupply == 0 || primaryPortfolioValue == 0) {
+            uint256 mintAmount = secondaryPortfolioValue / 100;
+            IndexToken token = factoryStorage.token();
+            token.mint(requester, mintAmount);
+            emit Issuanced(
+                _issuanceNonce,
+                requester,
+                factoryStorage.usdc(),
+                factoryStorage.issuanceInputAmount(_issuanceNonce),
+                mintAmount,
+                factoryStorage.getIndexTokenPrice(),
+                block.timestamp
+            );
+        } else {
+            uint256 secondaryTotalSupply = primaryTotalSupply * secondaryPortfolioValue / primaryPortfolioValue;
+            uint256 mintAmount = secondaryTotalSupply - primaryTotalSupply;
+            IndexToken token = factoryStorage.token();
+            token.mint(requester, mintAmount);
+            emit Issuanced(
+                _issuanceNonce,
+                requester,
+                factoryStorage.usdc(),
+                factoryStorage.issuanceInputAmount(_issuanceNonce),
+                mintAmount,
+                factoryStorage.getIndexTokenPrice(),
+                block.timestamp
+            );
+        }
+        factoryStorage.setIssuanceIsCompleted(_issuanceNonce, true);
+    }
+
+    function completeCancelIssuance(uint256 _issuanceNonce) public nonReentrant whenNotPaused {
+        require(factoryStorage.checkCancelIssuanceStatus(_issuanceNonce), "Cancel issuance is not completed");
+        require(!factoryStorage.cancelIssuanceComplted(_issuanceNonce), "The process has been completed before");
+        address requester = factoryStorage.issuanceRequesterByNonce(_issuanceNonce);
+        uint256 totalBalance;
+        IOrderProcessor issuer = factoryStorage.issuer();
+        for (uint256 i; i < functionsOracle.totalCurrentList(); i++) {
+            address tokenAddress = functionsOracle.currentList(i);
+            uint256 requestId = factoryStorage.issuanceRequestId(_issuanceNonce, tokenAddress);
+            uint256 cancelRequestId = factoryStorage.cancelIssuanceRequestId(_issuanceNonce, tokenAddress);
+            uint256 balance;
+            if (cancelRequestId > 0) {
+                uint256 feeTaken = issuer.getFeesTaken(cancelRequestId);
+                uint256 receivedAmount = issuer.getReceivedAmount(cancelRequestId);
+                balance += receivedAmount - feeTaken;
+            }
+            uint256 unfilledAmount = factoryStorage.cancelIssuanceUnfilledAmount(_issuanceNonce, tokenAddress);
+            totalBalance += (balance + unfilledAmount);
+        }
+        OrderManager orderManager = factoryStorage.orderManager();
+        orderManager.withdrawFunds(factoryStorage.usdc(), requester, totalBalance);
+        factoryStorage.setCancelIssuanceComplted(_issuanceNonce, true);
+        emit IssuanceCancelled(
+            _issuanceNonce,
+            requester,
+            factoryStorage.usdc(),
+            factoryStorage.issuanceInputAmount(_issuanceNonce),
+            0,
+            block.timestamp
+        );
+    }
+
+    function completeRedemption(uint256 _redemptionNonce) public nonReentrant whenNotPaused {
+        require(factoryStorage.checkRedemptionOrdersStatus(_redemptionNonce), "Redemption orders are not completed");
+        require(!factoryStorage.redemptionIsCompleted(_redemptionNonce), "Redemption is completed");
+        address requester = factoryStorage.redemptionRequesterByNonce(_redemptionNonce);
+        IOrderProcessor issuer = factoryStorage.issuer();
+        uint256 totalBalance;
+        for (uint256 i; i < functionsOracle.totalCurrentList(); i++) {
+            address tokenAddress = functionsOracle.currentList(i);
+            uint256 tokenRequestId = factoryStorage.redemptionRequestId(_redemptionNonce, tokenAddress);
+            uint256 balance = issuer.getReceivedAmount(tokenRequestId);
+            uint256 feeTaken = issuer.getFeesTaken(tokenRequestId);
+            totalBalance += balance - feeTaken;
+        }
+        uint256 fee = (totalBalance * factoryStorage.feeRate()) / 10000;
+        OrderManager orderManager = factoryStorage.orderManager();
+        orderManager.withdrawFunds(factoryStorage.usdc(), factoryStorage.feeReceiver(), fee);
+        orderManager.withdrawFunds(factoryStorage.usdc(), requester, totalBalance - fee);
+        factoryStorage.setRedemptionIsCompleted(_redemptionNonce, true);
+        emit Redemption(
+            _redemptionNonce,
+            requester,
+            factoryStorage.usdc(),
+            factoryStorage.redemptionInputAmount(_redemptionNonce),
+            totalBalance,
+            factoryStorage.getIndexTokenPrice(),
+            block.timestamp
+        );
+    }
+
+    function completeCancelRedemption(uint256 _redemptionNonce) public nonReentrant whenNotPaused {
+        require(factoryStorage.checkCancelRedemptionStatus(_redemptionNonce), "Cancel redemption is not completed");
+        require(!factoryStorage.cancelRedemptionComplted(_redemptionNonce), "The process has been completed before");
+
+        address requester = factoryStorage.redemptionRequesterByNonce(_redemptionNonce);
+        IOrderProcessor issuer = factoryStorage.issuer();
+        for (uint256 i; i < functionsOracle.totalCurrentList(); i++) {
+            address tokenAddress = functionsOracle.currentList(i);
+            uint256 tokenRequestId = factoryStorage.cancelRedemptionRequestId(_redemptionNonce, tokenAddress);
+            uint256 filledAmount = issuer.getReceivedAmount(tokenRequestId) - issuer.getFeesTaken(tokenRequestId);
+            uint256 unFilledAmount = factoryStorage.cancelRedemptionUnfilledAmount(_redemptionNonce, tokenAddress);
+            uint256 totalBalance = filledAmount + unFilledAmount;
+            if (totalBalance > 0) {
+                OrderManager orderManager = factoryStorage.orderManager();
+                orderManager.withdrawFunds(tokenAddress, address(this), totalBalance);
+                IERC20(tokenAddress).approve(factoryStorage.wrappedDshareAddress(tokenAddress), totalBalance);
+                WrappedDShare(factoryStorage.wrappedDshareAddress(tokenAddress)).deposit(
+                    totalBalance, address(factoryStorage.vault())
+                );
+            }
+        }
+        IndexToken token = factoryStorage.token();
+        token.mint(requester, factoryStorage.burnedTokenAmountByNonce(_redemptionNonce));
+        factoryStorage.setCancelRedemptionComplted(_redemptionNonce, true);
+        emit RedemptionCancelled(
+            _redemptionNonce,
+            requester,
+            factoryStorage.usdc(),
+            factoryStorage.redemptionInputAmount(_redemptionNonce),
+            0,
+            block.timestamp
+        );
+    }
+
+    function checkMultical(uint256 _reqeustId) public view returns (bool) {
+        IndexFactoryStorage.ActionInfo memory actionInfo = factoryStorage.getActionInfoById(_reqeustId);
+        if (actionInfo.actionType == 1) {
+            return factoryStorage.checkIssuanceOrdersStatus(actionInfo.nonce);
+        } else if (actionInfo.actionType == 2) {
+            return factoryStorage.checkRedemptionOrdersStatus(actionInfo.nonce);
+        } else if (actionInfo.actionType == 3) {
+            return factoryStorage.checkCancelIssuanceStatus(actionInfo.nonce);
+        } else if (actionInfo.actionType == 4) {
+            return factoryStorage.checkCancelRedemptionStatus(actionInfo.nonce);
+        }
+        return false;
+    }
+
+    function multical(uint256 _requestId) public {
+        IndexFactoryStorage.ActionInfo memory actionInfo = factoryStorage.getActionInfoById(_requestId);
+        if (actionInfo.actionType == 1) {
+            completeIssuance(actionInfo.nonce);
+        } else if (actionInfo.actionType == 2) {
+            completeRedemption(actionInfo.nonce);
+        } else if (actionInfo.actionType == 3) {
+            completeCancelIssuance(actionInfo.nonce);
+        } else if (actionInfo.actionType == 4) {
+            completeCancelRedemption(actionInfo.nonce);
+        }
+    }
+}
---
+++ src/stock/OrderManager.sol
@@ -0,0 +1,217 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+import "./dinari/interfaces/IOrderProcessor.sol";
+import {FeeLib} from "./dinari/common/FeeLib.sol";
+
+/// @title Order Manager
+/// @author NEX Labs Protocol
+/// @notice Allows User to initiate burn/mint requests and allows issuers to approve or deny them
+contract OrderManager is Initializable, OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable {
+    enum RequestStatus {
+        NULL,
+        PENDING,
+        CANCELED,
+        APPROVED,
+        REJECTED
+    }
+
+    event FundsWithdrawn(address token, address to, uint256 amount);
+
+    struct Request {
+        address requester; // sender of the request.
+        uint256 amount; // amount of token to mint/burn.
+        address[] depositAddresses; // issuer's asset address in mint, merchant's asset address in burn.
+        uint256 nonce; // serial number allocated for each request.
+        uint256 timestamp; // time of the request creation.
+        RequestStatus status; // status of the request.
+    }
+
+    IOrderProcessor public issuer;
+
+    address public usdc;
+    uint8 public usdcDecimals;
+
+    mapping(address => bool) public isOperator;
+
+    event BuyRequest(uint256 indexed id, uint256 time, uint256 inutAmount);
+    event SellRequest(uint256 indexed id, uint256 time, uint256 inutAmount);
+
+    function initialize(address _usdc, uint8 _usdcDecimals, address _issuer) external initializer {
+        require(_usdc != address(0), "invalid token address");
+        require(_issuer != address(0), "invalid issuer address");
+        require(_usdcDecimals > 0, "invalid decimals");
+        usdc = _usdc;
+        usdcDecimals = _usdcDecimals;
+        issuer = IOrderProcessor(_issuer);
+        __Ownable_init(msg.sender);
+        __Pausable_init();
+        __ReentrancyGuard_init();
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function setIssuer(address _issuer) external onlyOwner {
+        require(_issuer != address(0), "invalid issuer address");
+        issuer = IOrderProcessor(_issuer);
+    }
+
+    function setUsdcAddress(address _usdc, uint8 _usdcDecimals) public onlyOwner returns (bool) {
+        require(_usdc != address(0), "invalid token address");
+        usdc = _usdc;
+        usdcDecimals = _usdcDecimals;
+        return true;
+    }
+
+    function setOperator(address _operator, bool _status) public onlyOwner {
+        isOperator[_operator] = _status;
+    }
+
+    function getPrimaryOrder(bool sell) internal view returns (IOrderProcessor.Order memory) {
+        return IOrderProcessor.Order({
+            requestTimestamp: uint64(block.timestamp),
+            recipient: address(this),
+            assetToken: address(0),
+            paymentToken: address(usdc),
+            sell: sell,
+            orderType: IOrderProcessor.OrderType.MARKET,
+            assetTokenQuantity: sell ? 100 ether : 0,
+            paymentTokenQuantity: sell ? 0 : 100 ether,
+            price: 0,
+            tif: IOrderProcessor.TIF.GTC
+        });
+    }
+
+    function calculateFees(uint256 orderAmount) public view returns (uint256) {
+        (uint256 flatFee, uint24 percentageFeeRate) = issuer.getStandardFees(false, address(usdc));
+        uint256 fees = flatFee + FeeLib.applyPercentageFee(percentageFeeRate, orderAmount);
+        return fees;
+    }
+
+    function requestBuyOrder(address _token, uint256 _orderAmount, address _receiver)
+        external
+        nonReentrant
+        whenNotPaused
+        returns (uint256)
+    {
+        require(_token != address(0), "invalid token address");
+        require(_receiver != address(0), "invalid address");
+        require(_orderAmount > 0, "amount must be greater than 0");
+        require(isOperator[msg.sender] || msg.sender == owner(), "Not authorized Sender For Buy And Sell");
+        uint256 fees = calculateFees(_orderAmount);
+
+        IOrderProcessor.Order memory order = getPrimaryOrder(false);
+        order.recipient = _receiver;
+        order.assetToken = address(_token);
+        order.paymentTokenQuantity = _orderAmount;
+        uint256 quantityIn = order.paymentTokenQuantity + fees;
+
+        require(IERC20(usdc).transferFrom(msg.sender, address(this), quantityIn), "Transfer failed");
+        IERC20(usdc).approve(address(issuer), quantityIn);
+
+        uint256 id = issuer.createOrderStandardFees(order);
+        // orderInstanceById[id] = order;
+        emit BuyRequest(id, block.timestamp, _orderAmount);
+        return id;
+        // return 1;
+    }
+
+    function requestBuyOrderFromCurrentBalance(address _token, uint256 _orderAmount, address _receiver)
+        external
+        nonReentrant
+        whenNotPaused
+        returns (uint256)
+    {
+        require(_token != address(0), "invalid token address");
+        require(_receiver != address(0), "invalid address");
+        require(_orderAmount > 0, "amount must be greater than 0");
+        require(isOperator[msg.sender] || msg.sender == owner(), "Not authorized Sender For Buy And Sell");
+        uint256 fees = calculateFees(_orderAmount);
+
+        IOrderProcessor.Order memory order = getPrimaryOrder(false);
+        order.recipient = _receiver;
+        order.assetToken = address(_token);
+        order.paymentTokenQuantity = _orderAmount;
+        uint256 quantityIn = order.paymentTokenQuantity + fees;
+
+        IERC20(usdc).approve(address(issuer), quantityIn);
+
+        uint256 id = issuer.createOrderStandardFees(order);
+        // orderInstanceById[id] = order;
+        emit BuyRequest(id, block.timestamp, _orderAmount);
+        return id;
+        // return 1;
+    }
+
+    function requestSellOrder(address _token, uint256 _amount, address _receiver)
+        external
+        nonReentrant
+        whenNotPaused
+        returns (uint256)
+    {
+        require(_token != address(0), "invalid token address");
+        require(_receiver != address(0), "invalid address");
+        require(_amount > 0, "amount must be greater than 0");
+        require(isOperator[msg.sender] || msg.sender == owner(), "Not authorized Sender For Buy And Sell");
+
+        IOrderProcessor.Order memory order = getPrimaryOrder(true);
+        order.assetToken = _token;
+        order.assetTokenQuantity = _amount;
+        order.recipient = _receiver;
+
+        require(IERC20(_token).transferFrom(msg.sender, address(this), _amount), "Transfer failed");
+
+        IERC20(_token).approve(address(issuer), _amount);
+        uint256 id = issuer.createOrderStandardFees(order);
+
+        return id;
+    }
+
+    function requestSellOrderFromCurrentBalance(address _token, uint256 _amount, address _receiver)
+        external
+        nonReentrant
+        whenNotPaused
+        returns (uint256)
+    {
+        require(_token != address(0), "invalid token address");
+        require(_receiver != address(0), "invalid address");
+        require(_amount > 0, "amount must be greater than 0");
+        require(isOperator[msg.sender] || msg.sender == owner(), "Not authorized Sender For Buy And Sell");
+
+        IOrderProcessor.Order memory order = getPrimaryOrder(true);
+        order.assetToken = _token;
+        order.assetTokenQuantity = _amount;
+        order.recipient = _receiver;
+
+        IERC20(_token).approve(address(issuer), _amount);
+
+        uint256 id = issuer.createOrderStandardFees(order);
+
+        return id;
+    }
+
+    function withdrawFunds(address _token, address _to, uint256 _amount) external {
+        require(_token != address(0), "invalid token address");
+        require(_to != address(0), "invalid address");
+        require(_amount > 0, "amount must be greater than 0");
+        require(isOperator[msg.sender] || msg.sender == owner(), "Not authorized Sender For Buy And Sell");
+        emit FundsWithdrawn(_token, _to, _amount);
+        require(IERC20(_token).transfer(_to, _amount), "Transfer failed");
+    }
+
+    function cancelOrder(uint256 _requestId) external whenNotPaused {
+        require(_requestId > 0, "Invalid Request Id");
+        require(isOperator[msg.sender] || msg.sender == owner(), "Not authorized Sender For Buy And Sell");
+        issuer.requestCancel(_requestId);
+    }
+}
