a72f39f85d4da0b666df4d6955eba2a106b4cd02
Morteza3123 <mkhedry3123@gmail.com>
2025-09-30 19:31:29 +0330

    added price oracle

--- src/ccip/PriceOracle.sol
+++ src/ccip/PriceOracle.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-// pragma solidity >=0.5.0 <0.8.25;
-pragma solidity ^0.8.25;
+// pragma solidity >=0.5.0 <0.8.26;
+pragma solidity >=0.5.0;
+// pragma solidity ^0.8.25;
 
 import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
