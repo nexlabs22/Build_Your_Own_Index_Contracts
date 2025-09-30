b402d585eecd332f236741501c489abbf3c50963
Morteza3123 <mkhedry3123@gmail.com>
2025-10-01 01:10:51 +0330

    added ask values

--- src/factory/IndexFactoryBalancer.sol
+++ src/factory/IndexFactoryBalancer.sol
@@ -1,96 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
-import "../utils/proposable/ProposableOwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../ccip/MainChainStorage.sol";
-import "../oracle/FunctionsOracle.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "../libraries/SwapHelpers.sol";
-import "../interfaces/IWETH.sol";
-import "../ccip/BalancerSender.sol";
-import "../ccip/MainChainFactory.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-/// @title Index Token
-/// @author NEX Labs Protocol
-/// @notice The main token contract for Index Token (NEX Labs Protocol)
-/// @dev This contract uses an upgradeable pattern
-contract IndexFactoryBalancer is Initializable, ProposableOwnableUpgradeable, PausableUpgradeable {
-    MainChainStorage public mainChainStorage;
+import {FunctionsOracle} from "../oracle/FunctionsOracle.sol";
+import {MainChainBalancer} from "../ccip/MainChainBalancer.sol";
+import {IndexFactoryStorage} from "./IndexFactoryStorage.sol";
+import {Vault} from "../vault/Vault.sol";
+import {FeeCalculation} from "../libraries/FeeCalculation.sol";
+
+error ZeroAmount();
+error ZeroAddress();
+error WrongETHAmount();
+
+contract IndexFactoryBalancer is
+    Initializable,
+    OwnableUpgradeable,
+    PausableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+    using SafeERC20 for IERC20;
+
     FunctionsOracle public functionsOracle;
-    BalancerSender public balancerSender;
+    IndexFactoryStorage public factoryStorage;
+    MainChainBalancer public mainChainBalancer;
+    
 
-    uint64 public currentChainSelector;
+    uint256 private constant SHARE_DENOMINATOR = 100e18;
 
-    IWETH public weth;
+    mapping(uint256 => uint256) public portfolioTotalValueByNonce; // mapping of updatePortfolioNonce to total value
 
-    struct LowSwapVariables {
-        address tokenAddress;
-        uint256 tokenMarketShare;
-        uint256 chainValue;
-        uint256 swapWethAmount;
-        uint256 wethAmount;
-    }
-
-    struct ExtraSwapVariables {
-        address tokenAddress;
-        uint256 tokenMarketShare;
-        uint256 chainValue;
-        uint256 swapWethAmount;
-    }
-
-    event RequestedAskValues(uint256 time);
-    event RequestedFirstReweightAction(uint256 time);
-    event RequestedSecondReweightAction(uint256 time);
-
-    /**
-     * @dev Pauses the contract.
-     */
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    /**
-     * @dev Unpauses the contract.
-     */
-    function unpause() external onlyOwner {
-        _unpause();
-    }
-
-    modifier onlyOwnerOrOperator() {
-        require(msg.sender == owner() || functionsOracle.isOperator(msg.sender), "Caller is not the owner or operator");
-        _;
-    }
-
-    /**
-     * @dev Initializes the contract with the given parameters.
-     * @param _currentChainSelector The current chain selector.
-     * @param _mainChainStorage The address of the MainChainStorage contract.
-        * @param _functionsOracle The address of the FunctionsOracle contract.
-        * @param _balancerSender The address of the BalancerSender contract.
-     * @param _weth The address of the WETH token.
-     */
     function initialize(
-        uint64 _currentChainSelector,
-        address _mainChainStorage,
         address _functionsOracle,
-        address payable _balancerSender,
-        //addresses
-        address _weth
+        address _factoryStorage,
+        address _mainChainBalancer
     ) external initializer {
-        // Validate input parameters
-        require(_currentChainSelector > 0, "Invalid chain selector");
-        require(_mainChainStorage != address(0), "Invalid factory storage address");
-        require(_weth != address(0), "Invalid WETH address");
-        __Ownable_init(msg.sender);
-        //set chain selector
-        currentChainSelector = _currentChainSelector;
-        mainChainStorage = MainChainStorage(_mainChainStorage);
+        require(
+            _functionsOracle != address(0),
+            "Invalid address for _functionsOracle"
+        );
         functionsOracle = FunctionsOracle(_functionsOracle);
-        balancerSender = BalancerSender(_balancerSender);
-        //set addresses
-        weth = IWETH(_weth);
+        factoryStorage = IndexFactoryStorage(_factoryStorage);
+        mainChainBalancer = MainChainBalancer(_mainChainBalancer);
+
+        __Ownable_init(msg.sender);
+        __Pausable_init();
+        __ReentrancyGuard_init();
     }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -98,51 +58,21 @@ contract IndexFactoryBalancer is Initializable, ProposableOwnableUpgradeable, Pa
         _disableInitializers();
     }
 
-    /**
-     * @dev Sets the MainChainStorage contract address.
-     * @param _mainChainStorage The address of the MainChainStorage contract.
-     */
-    function setMainChainStorage(address _mainChainStorage) public onlyOwner {
-        mainChainStorage = MainChainStorage(_mainChainStorage);
+    function increasePortfolioTotalValueByNonce(uint256 _updatePortfolioNonce, uint256 _totalValue)
+        public
+        onlyMainChainBalancer
+    {
+        portfolioTotalValueByNonce[_updatePortfolioNonce] += _totalValue;
     }
 
-    /**
-     * @dev Sets the FunctionsOracle contract address.
-     * @param _functionsOracle The address of the FunctionsOracle contract.
-     */
-    function setFunctionsOracle(address _functionsOracle) public onlyOwner {
-        functionsOracle = FunctionsOracle(_functionsOracle);
-    }
-
-    function setBalancerSender(address payable _balancerSender) public onlyOwner {
-        balancerSender = BalancerSender(_balancerSender);
-    }
-
-    // set WETH address
-    function setWethAddress(address _weth) public onlyOwner {
-        require(_weth != address(0), "Invalid WETH address");
-        weth = IWETH(_weth);
-    }
-
-    /**
-     * @dev Sets the current chain selector.
-     * @param _currentChainSelector The current chain selector.
-     */
-    function setCurrentChainSelector(uint64 _currentChainSelector) public onlyOwner {
-        require(_currentChainSelector > 0, "Invalid chain selector");
-        currentChainSelector = _currentChainSelector;
-    }
-
-    // pause main chain factory when rebalance happens
-    function pauseMainChainFactory() public onlyOwnerOrOperator {
-        address mainChainFactoryAddress = mainChainStorage.mainChainFactory();
-        MainChainFactory mainChainFactory = MainChainFactory(payable(mainChainFactoryAddress));
-        if (!mainChainFactory.paused()) {
-            mainChainFactory.pause();
-        }
-    }
+    // =========================
+    // === External Functions ==
+    // =========================
 
     
-    
-    
+    function askValueCCIP(
+        address _indexToken
+    ) external whenNotPaused nonReentrant returns (uint256 orderNonce) {
+        mainChainBalancer.askValues(_indexToken);
+    }
 }
